bcfcd67982e68b115e10724d6deea431d49c27cb
刘竞 <jing.liu14@ucarinc.com>
2020-05-12 14:25:48 +0800

    初始化

---
+++ udfs-backend/src/main/java/com/moonlight035/udfsbackend/UdfsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moonlight035.udfsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdfsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdfsBackendApplication.class, args);
+    }
+
+}
---
+++ udfs-backend/src/test/java/com/moonlight035/udfsbackend/UdfsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonlight035.udfsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdfsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
